e5c30476412e03a4cce589f3fb0563bf4d621c9c
Brunalu28 <bluiza747@gmail.com>
2025-07-20 14:50:50 -0300

    Adicionando repository

---
+++ src/main/java/com/panicButton/panicButton/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.panicButton.panicButton.repository;
+
+import com.panicButton.panicButton.domain.Usuario;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+}
